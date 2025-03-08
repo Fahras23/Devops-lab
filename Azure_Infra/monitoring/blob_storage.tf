9d85bf3e4108ea0fba55afd9da90b662d543eb89
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-03-08 17:07:10 +0100

    fix resource

--- Azure_Infra/monitoring/blob_storage.tf
+++ Azure_Infra/monitoring/blob_storage.tf
@@ -1,3 +1,11 @@
+resource "azurerm_storage_account" "monitoring" {
+  name                     = "kubernetes"
+  resource_group_name      = "MC_Infrastructure_chatapp-uw2_westus2"
+  location                 = "West US 2"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
 resource "azurerm_storage_container" "kube_logs" {
   name                  = "kube-logs"
   storage_account_name    = "MC_Infrastructure_chatapp-uw2_westus2"
