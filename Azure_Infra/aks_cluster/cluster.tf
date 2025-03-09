6df89e90f11c936497667fb27edf911141d70f82
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-03-09 20:10:42 +0100

    change vm size

--- Azure_Infra/aks_cluster/cluster.tf
+++ Azure_Infra/aks_cluster/cluster.tf
@@ -6,7 +6,7 @@ resource "azurerm_kubernetes_cluster" "chatapp_cluster" {
 
   default_node_pool {
     name       = "chatapp"
-    vm_size    = "Standard_A2_v2"
+    vm_size    = "Standard_DA2_v2"
     auto_scaling_enabled = true
     min_count           = 1   
     max_count           = 1   
