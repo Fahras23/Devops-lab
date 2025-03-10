5b0e5151107b4e9e184ed11056831dd91c1825be
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-03-10 13:41:24 +0100

    test node maintanance for pg cluster

--- Azure_Infra/aks_cluster/cluster.tf
+++ Azure_Infra/aks_cluster/cluster.tf
@@ -6,7 +6,7 @@ resource "azurerm_kubernetes_cluster" "chatapp_cluster" {
 
   default_node_pool {
     name       = "chatapp"
-    vm_size    = "Standard_D2s_v3"
+    vm_size    = "Standard_A2_v2"
     auto_scaling_enabled = true
     min_count           = 1   
     max_count           = 1
