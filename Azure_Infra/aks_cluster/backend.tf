a67b6558b2242b2f29799c41cbc00f048111697e
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-03-16 20:56:47 +0100

    update creds

--- Azure_Infra/aks_cluster/backend.tf
+++ Azure_Infra/aks_cluster/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
       resource_group_name  = "Infrastructure"
-      storage_account_name = "tfstatestoragewu2"
+      storage_account_name = "tfstoragebucket"
       container_name       = "tfstate"
       key                  = "aks/terraform.tfstate"
   }
