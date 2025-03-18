ef70e51c6604809fdc617661f1383e713dcc4d6e
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-03-18 08:58:45 +0100

    update cluster config and vnet

--- Azure_Infra/aks_cluster/vnet.tf
+++ Azure_Infra/aks_cluster/vnet.tf
@@ -19,13 +19,13 @@ resource "azurerm_virtual_network" "chat-vnet" {
 resource "azurerm_subnet" "subnet1" {
   name                 = "subnet1"
   resource_group_name  = "Infrastructure"
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  virtual_network_name = azurerm_virtual_network.chat-vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_subnet" "subnet2" {
   name                 = "subnet2"
   resource_group_name  = "Infrastructure"
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  virtual_network_name = azurerm_virtual_network.chat-vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
