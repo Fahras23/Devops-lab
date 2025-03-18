d06c50dcf6db932d22a7f25f14f6c4f9651eaca8
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-03-18 10:11:39 +0100

    update netowrking

--- Azure_Infra/aks_cluster/cluster.tf
+++ Azure_Infra/aks_cluster/cluster.tf
@@ -18,7 +18,7 @@ resource "azurerm_kubernetes_cluster" "chatapp_cluster" {
  network_profile {
     network_plugin = "azure"
     network_plugin_mode = "overlay"
-    pod_cidr = "10.0.0.2/24"
+    pod_cidr = "10.0.2.0/24"
   }
 
   identity {
