e845adaa0322e8e44f1af8ed888b9e4df5d34855
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-03-19 16:45:57 +0100

    increase node number

--- Azure_Infra/aks_cluster/cluster.tf
+++ Azure_Infra/aks_cluster/cluster.tf
@@ -7,10 +7,9 @@ resource "azurerm_kubernetes_cluster" "chatapp_cluster" {
   default_node_pool {
     name       = "chatapp"
     vm_size    = "Standard_D2s_v4"
-    auto_scaling_enabled = true
+    auto_scaling_enabled = false
     min_count           = 2 
     max_count           = 3
-    node_count          = 2
     temporary_name_for_rotation = "maintenance"
   }
 
