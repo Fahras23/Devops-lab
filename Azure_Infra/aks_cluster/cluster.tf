4d3f4efc0310a406aac1f2eac454f4b8031bfcba
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-03-20 15:16:32 +0100

    change node scaling numbers

--- Azure_Infra/aks_cluster/cluster.tf
+++ Azure_Infra/aks_cluster/cluster.tf
@@ -7,9 +7,8 @@ resource "azurerm_kubernetes_cluster" "chatapp_cluster" {
   default_node_pool {
     name       = "chatapp"
     vm_size    = "Standard_D2s_v4"
-    auto_scaling_enabled = true
-    min_count           = 2 
-    max_count           = 3
+    auto_scaling_enabled = false
+    node_count = 2
     temporary_name_for_rotation = "maintenance"
   }
 
