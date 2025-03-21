409aeb4106179c671a8077cfda884cc75e5b7eed
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-03-21 18:23:32 +0100

    crate policy assigment for cluster

--- Azure_Infra/aks_cluster/policy.tf
+++ Azure_Infra/aks_cluster/policy.tf
@@ -15,3 +15,9 @@ METADATA
   policy_rule = templatefile("${path.module}/templates/policy_rule.tpl", {})
 
 }
+
+resource "azurerm_resource_policy_assignment" "rule_assigm" {
+  name                 = "aks-policy-assignment"
+  resource_id          = azurerm_kubernetes_cluster.chatapp_cluster.id
+  policy_definition_id = azurerm_policy_definition.example.id
+}
