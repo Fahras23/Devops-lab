56f45332f5b2ccc9000471e077a62b02323dbf41
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-03-21 18:29:23 +0100

    remove not used vnet

--- Azure_Infra/aks_cluster/vnet.tf
+++ Azure_Infra/aks_cluster/vnet.tf
@@ -1,24 +1,24 @@
-resource "azurerm_network_security_group" "chat-nsg" {
-  name                = "chat-cluster-nsg"
-  location            = "West US 2"
-  resource_group_name = "Infrastructure"
-}
+# resource "azurerm_network_security_group" "chat-nsg" {
+#   name                = "chat-cluster-nsg"
+#   location            = "West US 2"
+#   resource_group_name = "Infrastructure"
+# }
 
-resource "azurerm_virtual_network" "chat-vnet" {
-  name                = "chat-cluster-vnet"
-  location            = "West US 2"
-  resource_group_name = "Infrastructure"
-  address_space       = ["10.0.0.0/20"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+# resource "azurerm_virtual_network" "chat-vnet" {
+#   name                = "chat-cluster-vnet"
+#   location            = "West US 2"
+#   resource_group_name = "Infrastructure"
+#   address_space       = ["10.0.0.0/20"]
+#   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
-  tags = {
-    environment = "Kubernetes"
-  }
-}
+#   tags = {
+#     environment = "Kubernetes"
+#   }
+# }
 
-resource "azurerm_subnet" "subnet1" {
-  name                 = "subnet1"
-  resource_group_name  = "Infrastructure"
-  virtual_network_name = azurerm_virtual_network.chat-vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
+# resource "azurerm_subnet" "subnet1" {
+#   name                 = "subnet1"
+#   resource_group_name  = "Infrastructure"
+#   virtual_network_name = azurerm_virtual_network.chat-vnet.name
+#   address_prefixes     = ["10.0.1.0/24"]
+# }
