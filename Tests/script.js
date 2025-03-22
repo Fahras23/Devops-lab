66446fb77e1663cc530c966e43eaa28c1d811b9b
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-03-22 22:09:16 +0100

    fix memory scaling condition

--- Tests/script.js
+++ Tests/script.js
@@ -3,19 +3,14 @@ import { sleep } from 'k6';
 
 export const options = {
   // A number specifying the number of VUs to run concurrently.
-  vus: 50,
+  vus: 10,
+  // loop iterations
+  iterations: 10 ,
   // A string specifying the total duration of the test run.
-  duration: '30s',
+  duration: '10s',
 };
 
 export default function() {
   http.get(`http://${__ENV.HOSTNAME}/`);
   sleep(1);
-  http.get(`http://${__ENV.HOSTNAME}/`);
-  sleep(1);
-  http.get(`http://${__ENV.HOSTNAME}/`);
-  sleep(1);
-  http.get(`http://${__ENV.HOSTNAME}/`);
-  sleep(1);
-  http.get(`http://${__ENV.HOSTNAME}/`);
 }
