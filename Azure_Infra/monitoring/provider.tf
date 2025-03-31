803d75a5eabb4030e2f5f245af9227f87b65d226
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-04-01 00:49:53 +0200

    add grafana in tf

--- Azure_Infra/monitoring/provider.tf
+++ Azure_Infra/monitoring/provider.tf
@@ -4,10 +4,17 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=4.4.0"
     }
+    grafana = {
+      source = "grafana/grafana"
+      version = ">= 2.0.0"
+    }
   }
 }
 
 provider "azurerm" {
   features {}
+}
+
+provider "grafana" {
 
 }
