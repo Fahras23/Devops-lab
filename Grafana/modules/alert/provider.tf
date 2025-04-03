8d6394faac3f5ea1e6a2977e62a84516ba1d3886
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-04-04 00:17:40 +0200

    test module

--- Grafana/modules/alert/provider.tf
+++ Grafana/modules/alert/provider.tf
@@ -1,1 +1,27 @@
-provider.tf
+terraform {
+  required_providers {
+    grafana = {
+      source = "grafana/grafana"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "grafana" {
+  url  = var.grafana_url
+  auth = var.grafana_auth
+}
+
+variable "grafana_url" {
+  type      = string
+  sensitive = true
+}
+
+variable "grafana_auth" {
+  type      = string
+  sensitive = true
+}
