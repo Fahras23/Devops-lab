8bf320da24bb3dc26c6ae203a6265e1aed05021f
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-04-04 00:27:58 +0200

    test module

--- Grafana/modules/alert/provider.tf
+++ Grafana/modules/alert/provider.tf
@@ -10,16 +10,4 @@ terraform {
 
 provider "grafana" {
   alias = "module"
-  url  = var.grafana_url
-  auth = var.grafana_auth
-}
-
-variable "grafana_url" {
-  type      = string
-  sensitive = true
-}
-
-variable "grafana_auth" {
-  type      = string
-  sensitive = true
 }
