ba8bcb10da8330e5341665993c9faa67d149cc4e
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-04-04 00:41:31 +0200

    test module

---
+++ Grafana/provider.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.4.0"
+    }
+    grafana = {
+      source = "grafana/grafana"
+      version = "3.20.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "grafana" {
+  alias = "usage"
+  url  = var.grafana_url
+  auth = var.grafana_auth
+}
+
+variable "grafana_url" {
+  type      = string
+  sensitive = true
+}
+
+variable "grafana_auth" {
+  type      = string
+  sensitive = true
+}
---
+++ Grafana/variables.tf
@@ -0,0 +1,9 @@
+variable "grafana_url" {
+  type      = string
+  sensitive = true
+}
+
+variable "grafana_auth" {
+  type      = string
+  sensitive = true
+}
