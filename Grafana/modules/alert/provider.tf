f693f199852cd835a04275302c448fbe47489f76
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-04-04 00:19:21 +0200

    test module

--- Grafana/modules/alert/provider.tf
+++ Grafana/modules/alert/provider.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    grafana = {
-      source = "grafana/grafana"
-      version = ">= 2.0.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 provider "grafana" {
   url  = var.grafana_url
   auth = var.grafana_auth
