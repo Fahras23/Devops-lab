98f3ed371960be1889eeb2ec017bc8ba6e4f7c37
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-04-04 14:26:06 +0200

    test module

--- Grafana/alert.tf
+++ Grafana/alert.tf
@@ -1,6 +1,9 @@
 module "example" {
     source = "./modules/alert"
     
+    grafana_url = var.grafana_url
+    grafana_url = var.grafana_auth
+    
     folder_name = "example"
     alert_name = "example"
 
--- Grafana/modules/alert/provider.tf
+++ Grafana/modules/alert/provider.tf
@@ -1,1 +1,21 @@
-provider.tf
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.4.0"
+    }
+    grafana = {
+      source = "grafana/grafana"
+      version = "3.20.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "grafana" {
+  url  = var.grafana_url
+  auth = var.grafana_auth
+}
