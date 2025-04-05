c839a7bb130681c47360f710110e0a484d028367
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-04-05 22:23:39 +0200

    setup count setting

--- Grafana/modules/alert/main.tf
+++ Grafana/modules/alert/main.tf
@@ -46,9 +46,27 @@ resource "grafana_rule_group" "my_alert_rule" {
         intervalMs    = 1000
         maxDataPoints = 43200
         refId         = "B"
-        query = {
-          params = ["sum(rate(node_cpu_seconds_total{node=\"aks-chatapp-33350159-vmss000000\"}[5m]))"]
-        }
+        conditions = [
+          {
+            evaluator = {
+              params = [3],
+              type   = "gt"
+            },
+            operator = {
+              type = "and"
+            },
+            query = {
+              params = [
+                "sum(rate(node_cpu_seconds_total{node=\"aks-chatapp-33350159-vmss000000\"}[5m]))"
+              ]
+            },
+            reducer = {
+              params = [],
+              type   = "last"
+            },
+            type = "query"
+          }
+        ],
       })
     }
   }
