0c87126bce1bb01ab76d143b51a8dd6889c698ad
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-04-05 21:26:56 +0200

    setup count setting

--- Grafana/modules/alert/main.tf
+++ Grafana/modules/alert/main.tf
@@ -46,42 +46,5 @@ resource "grafana_rule_group" "my_alert_rule" {
         to   = 0
       }
       datasource_uid = "-100"
-      model          = <<EOT
-{
-    "conditions": [
-        {
-        "evaluator": {
-            "params": [
-            3
-            ],
-            "type": "gt"
-        },
-        "operator": {
-            "type": "and"
-        },
-        "query": {
-            "params": [
-            "sum(rate(node_cpu_seconds_total{node="aks-chatapp-33350159-vmss000000"}[5m]))"
-            ]
-        },
-        "reducer": {
-            "params": [],
-            "type": "last"
-        },
-        "type": "query"
-        }
-    ],
-    "datasource": {
-        "type": "__expr__",
-        "uid": "-100"
-    },
-    "hide": false,
-    "intervalMs": 1000,
-    "maxDataPoints": 43200,
-    "refId": "B",
-    "type": "classic_conditions"
-}
-EOT
-    }
-  }
+      model          = templatefile("${path.module}/templates/alert_data.tpl", {})
 }
