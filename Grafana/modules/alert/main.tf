6e3b131de490ffea747e74edd63741249e4535df
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-04-06 15:38:41 +0200

    setup alert setting

--- Grafana/modules/alert/main.tf
+++ Grafana/modules/alert/main.tf
@@ -7,35 +7,35 @@ resource "grafana_rule_group" "my_alert_rule" {
   name             = "My Rule Group"
   folder_uid       = grafana_folder.rule_folder.uid
   org_id           = 1
-  interval_seconds = 240
+  interval_seconds = 60
 
   rule {
-    name      = "Namespace count changed"
-    condition = "B"
+    name      = "Namespaces"
+    condition = "C"
 
     data {
-      ref_id = "B"
+      ref_id = "A"
 
       relative_time_range {
-        from = 0
+        from = 600
         to   = 0
       }
 
       datasource_uid = "behtturun0phcf"
-      model          = templatefile("${path.module}/templates/alert_data.tpl", {})
+      model          = "{\"editorMode\":\"code\",\"expr\":\"sum(count(kube_namespace_created) by (namespace))\",\"instant\":true,\"intervalMs\":1000,\"legendFormat\":\"__auto\",\"maxDataPoints\":43200,\"range\":false,\"refId\":\"A\"}"
     }
 
     no_data_state  = "NoData"
-    exec_err_state = "Alerting"
-    for            = "2m"
-    annotations = {
-      a = "b"
-      c = "d"
+    exec_err_state = "Error"
+    for            = "1m"
+    annotations    = {}
+    labels         = {}
+    is_paused      = false
+
+    notification_settings {
+      contact_point = "grafana-default-email"
+      group_by      = null
+      mute_timings  = null
     }
-    labels = {
-      e = "f"
-      g = "h"
-    }
-    is_paused = false
   }
 }
