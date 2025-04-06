28cc7a5e8f52948eabc723aa35beabc5201f5d4c
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-04-06 16:11:04 +0200

    setup alert setting

--- Grafana/modules/alert/main.tf
+++ Grafana/modules/alert/main.tf
@@ -6,7 +6,7 @@ resource "grafana_contact_point" "email_alert" {
   name = "email-alerts"
 
   email {
-    addresses = ["2001krzysztoflyczak@gmail.com"]
+    addresses = ["2001krzysztoflyczak@@gmail.com"]
     message   = "Alert: {{ .CommonLabels.alertname }} - {{ .CommonAnnotations.summary }}"
     subject   = "Grafana Alert: {{ .CommonLabels.alertname }}"
   }
@@ -21,43 +21,53 @@ resource "grafana_rule_group" "my_alert_rule" {
 
   rule {
     name      = var.alert_name
-    condition = "A"  # Directly reference the query result
+    condition = "C"
 
-    # Single data block: Query + Threshold in one step
     data {
       ref_id = "A"
+
       relative_time_range {
         from = 600
         to   = 0
       }
-      datasource_uid = "behtturun0phcf"  # Your metrics datasource (e.g., Prometheus)
-      model = jsonencode({
-        expr          = "sum(count(kube_namespace_created) by (namespace))",
-        intervalMs    = 1000,
-        maxDataPoints = 43200,
-        refId         = "A",
-        # Embedded threshold condition
-        conditions = [{
-          evaluator = {
-            params = [9],  # Threshold value
-            type   = "gt"  # Alert if > 9
-          },
-          operator  = { type = "and" },
-          query     = { params = ["A"] },  # Self-reference
-          reducer   = { type = "last" },
-          type      = "query"
-        }]
-      })
+
+      datasource_uid = "behtturun0phcf"
+      model          = "{\"editorMode\":\"code\",\"expr\":\"sum(count(kube_namespace_created) by (namespace))\",\"instant\":true,\"intervalMs\":1000,\"legendFormat\":\"__auto\",\"maxDataPoints\":43200,\"range\":false,\"refId\":\"A\"}"
+    }
+    data {
+      ref_id = "B"
+
+      relative_time_range {
+        from = 600
+        to   = 0
+      }
+
+      datasource_uid = "__expr__"
+      model          = "{\"conditions\":[{\"evaluator\":{\"params\":[],\"type\":\"gt\"},\"operator\":{\"type\":\"and\"},\"query\":{\"params\":[\"B\"]},\"reducer\":{\"params\":[],\"type\":\"last\"},\"type\":\"query\"}],\"datasource\":{\"type\":\"__expr__\",\"uid\":\"__expr__\"},\"expression\":\"A\",\"intervalMs\":1000,\"maxDataPoints\":43200,\"reducer\":\"last\",\"refId\":\"B\",\"type\":\"reduce\"}"
+    }
+    data {
+      ref_id = "C"
+
+      relative_time_range {
+        from = 600
+        to   = 0
+      }
+
+      datasource_uid = "__expr__"
+      model          = "{\"conditions\":[{\"evaluator\":{\"params\":[9],\"type\":\"gt\"},\"operator\":{\"type\":\"and\"},\"query\":{\"params\":[\"C\"]},\"reducer\":{\"params\":[],\"type\":\"last\"},\"type\":\"query\"}],\"datasource\":{\"type\":\"__expr__\",\"uid\":\"__expr__\"},\"expression\":\"B\",\"intervalMs\":1000,\"maxDataPoints\":43200,\"refId\":\"C\",\"type\":\"threshold\"}"
     }
 
     no_data_state  = "NoData"
     exec_err_state = "Error"
     for            = "1m"
+    annotations    = {}
+    labels         = {}
     is_paused      = false
 
     notification_settings {
-    contact_point = grafana_contact_point.email_alert.name  # Reference the contact point
-    group_by      = ["..."]  # Optional: Group alerts by labels
+      contact_point = grafana_contact_point.email_alert.name
+      group_by      = null
+      mute_timings  = null
     }
   }
 }
