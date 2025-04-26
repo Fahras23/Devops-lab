5e252e54b407e968519d587fd207f7b3a1df28ab
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-04-26 14:04:50 +0200

    update cluster

--- Azure_Infra/aks_cluster/cluster.tf
+++ Azure_Infra/aks_cluster/cluster.tf
@@ -3,14 +3,15 @@ resource "azurerm_kubernetes_cluster" "chatapp_cluster" {
   location            = "West US 2"
   resource_group_name = "Infrastructure"
   dns_prefix          = "chat"
-  
+  oidc_issuer_enabled = false
+
   default_node_pool {
     name       = "chatapp"
     vm_size    = "Standard_D2s_v4"
     auto_scaling_enabled = true
-    min_count           = 2
-    max_count           = 3
-    node_count          = 2
+    min_count           = 1
+    max_count           = 2
+    node_count          = 1
     temporary_name_for_rotation = "maintenance"
   }
 
