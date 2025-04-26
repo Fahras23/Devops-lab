f39083ac10930a99a7371c636ba20d77a8fa273c
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-04-26 14:13:41 +0200

    update cluster

--- Grafana/backend.tf
+++ Grafana/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
       resource_group_name  = "Infrastructure"
-      storage_account_name = "tfstoragebucket"
+      storage_account_name = "kubestrg"
       container_name       = "tfstate"
       key                  = "grafana/terraform.tfstate"
   }
