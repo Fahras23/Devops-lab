69a8984682cb96f0f3c1d999046ffb8e408912c5
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-04-26 14:33:23 +0200

    remove policy

--- Azure_Infra/aks_cluster/policy.tf
+++ Azure_Infra/aks_cluster/policy.tf
@@ -1,23 +1,23 @@
-resource "azurerm_policy_definition" "aks_policy" {
-  name         = "VMSizePolicy"
-  policy_type  = "Custom"
-  mode         = "All"
-  display_name = "Allow only 1 type of vm size"
+# resource "azurerm_policy_definition" "aks_policy" {
+#   name         = "VMSizePolicy"
+#   policy_type  = "Custom"
+#   mode         = "All"
+#   display_name = "Allow only 1 type of vm size"
 
-  metadata = <<METADATA
-    {
-    "category": "AKS"
-    }
+#   metadata = <<METADATA
+#     {
+#     "category": "AKS"
+#     }
 
-METADATA
+# METADATA
 
 
-  policy_rule = templatefile("${path.module}/templates/policy_rule.tpl", {})
+#   policy_rule = templatefile("${path.module}/templates/policy_rule.tpl", {})
 
-}
+# }
 
-resource "azurerm_resource_policy_assignment" "rule_assigm" {
-  name                 = "aks-policy-assignment"
-  resource_id          = azurerm_kubernetes_cluster.chatapp_cluster.id
-  policy_definition_id = azurerm_policy_definition.aks_policy.id
-}
+# resource "azurerm_resource_policy_assignment" "rule_assigm" {
+#   name                 = "aks-policy-assignment"
+#   resource_id          = azurerm_kubernetes_cluster.chatapp_cluster.id
+#   policy_definition_id = azurerm_policy_definition.aks_policy.id
+# }
