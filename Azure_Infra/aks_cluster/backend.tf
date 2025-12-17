5a3b29ee61e6476b69e1e9c65d8c717272a81942
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-12-17 17:09:29 +0100

    change of azure account

--- Azure_Infra/aks_cluster/backend.tf
+++ Azure_Infra/aks_cluster/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-      resource_group_name  = "Infrastructure"
-      storage_account_name = "kubestrg"
+      resource_group_name  = "infra"
+      storage_account_name = "infrastrg"
       container_name       = "tfstate"
       key                  = "aks/terraform.tfstate"
   }
