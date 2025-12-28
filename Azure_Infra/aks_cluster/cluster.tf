0740b9a109d49cd8aec881c78ac582fb336729a8
Fahras23 <2001krzysztoflyczak@gmail.com>
2025-12-28 12:59:44 +0100

    setup spot node pool

--- Azure_Infra/aks_cluster/cluster.tf
+++ Azure_Infra/aks_cluster/cluster.tf
@@ -2,17 +2,14 @@ resource "azurerm_kubernetes_cluster" "chatapp_cluster" {
   name                = "chat-uw2"
   location            = "West US 2"
   resource_group_name = "infra"
-  dns_prefix          = "chat"
+  dns_prefix          = "infra"
   oidc_issuer_enabled = false
 
+  #Overwritten by spot node pool
   default_node_pool {
-    name       = "chatapp"
-    vm_size    = "Standard_D2s_v4"
-    auto_scaling_enabled = true
-    min_count           = 1
-    max_count           = 2
-    node_count          = 2
-    temporary_name_for_rotation = "maintenance"
+    name       = "default"
+    node_count = 1
+    vm_size = "Standard_D2s_v4"
   }
 
   identity {
@@ -20,6 +17,34 @@ resource "azurerm_kubernetes_cluster" "chatapp_cluster" {
   }
 
   tags = {
-    Environment = "Testing"
+    Environment = "Production"
   }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "spot" {
+  name = "spot"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.chatapp_cluster.id
+  
+  #General VM Config
+  vm_size = "Standard_D2s_v4"
+  os_type = "Linux"
+  node_count = 2
+  os_disk_type = "Ephemeral"
+  temporary_name_for_rotation = "maintenance"
+
+  #Cost Optimization with spot instances
+  priority = "Spot"
+  eviction_policy = "Delete"
+  spot_max_price = -1
+
+  #Autoscaling setup
+  auto_scaling_enabled = true
+  min_count = 2
+  max_count = 3
+
+  node_labels = {
+    "nodepool-type" = "spot"
+    "workload"      = "infra"
+  }
+
 }
